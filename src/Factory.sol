bbf60246cc302745c1ee435a4146901635c67d96
yug49 <148035793+yug49@users.noreply.github.com>
2025-05-18 14:11:45 +0530

    updates factory.sol

--- src/Factory.sol
+++ src/Factory.sol
@@ -9,8 +9,8 @@ contract Factory is Ownable {
 
     ConfidentialEscrow private escrow;
     address[] private escrowAddresses;
-    mapping(address buyers => address escrowAddresses) private escrowAddressesOfBuyer;
-    mapping(address sellers => address escrowAddresses) private escrowAddressesOfSeller;
+    mapping(address buyers => address[] escrowAddresses) private escrowAddressesOfBuyer;
+    mapping(address sellers => address[] escrowAddresses) private escrowAddressesOfSeller;
 
     mapping(address => address) private vaults;
 
@@ -28,7 +28,19 @@ contract Factory is Ownable {
         if (msg.sender != _buyer) revert NotBuyer();
         escrow = new ConfidentialEscrow(_seller, _buyer, _amount, _tokenAddress, _governor, _conditions, _deadline);
         escrowAddresses.push(address(escrow));
-        escrowAddressesOfBuyer[_buyer] = address(escrow);
-        escrowAddressesOfSeller[_seller] = address(escrow);
+        escrowAddressesOfBuyer[_buyer].push(address(escrow));
+        escrowAddressesOfSeller[_seller].push(address(escrow));
+    }
+
+    function getEscrowAddresses() external view returns (address[] memory) {
+        return escrowAddresses;
+    }
+
+    function getEscrowAddressOfBuyer(address _buyer) external view returns (address[] memory) {
+        return escrowAddressesOfBuyer[_buyer];
+    }
+
+    function getEscrowAddressesOfSeller(address _seller) external view returns (address[] memory) {
+        return escrowAddressesOfSeller[_seller];
     }
 }
